4b9af53cb9818447ca0ea73cf51db0c93ae7830c
Ielfran <gaganraiqpwo@gmail.com>
2025-08-12 21:41:21 +0545

    The views have been added

--- config/database.php
+++ config/database.php
@@ -31,10 +31,18 @@ return [
 
     'connections' => [
         'mongodb' => [
-            'driver' =>'mongodb',
-            'dsn' => env('DB_URI'),
-            'database' => env('DB_DATABASE', 'instagram-clone'),
+            'driver'   => 'mongodb',
+            'dsn'      => env('DB_DSN', ''),  // use DSN if available
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'options'  => [
+                'database' => env('DB_AUTH_DATABASE', 'admin'), // authentication DB
+            ],
         ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
